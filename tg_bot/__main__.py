d71b7ee69c88c69a1ffed3e40493f08b28ada8fd
KRSreekumar <67498221+KRSreekumar@users.noreply.github.com>
2020-08-16 22:22:47 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -132,7 +132,8 @@ def start(bot: Bot, update: Update, args: List[str]):
             first_name = update.effective_user.first_name
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
-                parse_mode=ParseMode.MARKDOWN,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="😬ADD YOUR GROUP😬", url="t.me/{}?startgroup=true".format(bot.username))]]))
+                parse_mode=ParseMode.MARKDOWN,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="😬 Add Groups 😬", url="t.me/{}?startgroup=true".format(bot.username)), InlineKeyboardButton(text="❓ Help ❓", url="t.me/{}?start=help".format(bot.username))],
+                                                                                 [InlineKeyboardButton(text="❤ My group ❤", url="t.me/KL35Cinemas"), InlinekeyboradButton(text="💛 My Channel 💛", url="t.me/KL35Cinemaz")]]))
     else:
         update.effective_message.reply_text("")
 
